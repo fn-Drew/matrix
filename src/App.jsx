56d1ddca623dd766fe12737b41d087280d380731
fn-Drew <alutter03@gmail.com>
2023-02-24 16:56:41 -0500

    added physics + objects with rapier

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,48 @@
 import './App.css'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, } from '@react-three/fiber'
 import { PointerLockControls } from '@react-three/drei'
+import { Suspense } from 'react'
+import { Physics, RigidBody } from '@react-three/rapier'
+
+
+function Floor() {
+
+    return (
+        <RigidBody>
+            <mesh position={[0, -2, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+                <planeGeometry args={[300, 300]} />
+            </mesh >
+        </RigidBody >
+    )
+
+}
+
+function Player() {
+
+    return (
+        <RigidBody>
+            <mesh >
+                <sphereGeometry />
+                <meshStandardMaterial color='orange' />
+            </mesh>
+        </RigidBody>
+    )
+
+}
 
 function App() {
 
     return (
         <div id="canvas-container">
-            <Canvas>
-                <perspectiveCamera makeDefault />
-                <PointerLockControls fov />
-                <ambientLight color='turquoise' intensity={0.2} />
-                <mesh>
-                    <boxGeometry args={[2, 2, 2]} />
-                    <meshStandardMaterial />
-                </mesh>
+            <Canvas >
+                <Suspense>
+                    <Physics gravity={[0, -9.8, 0]}>
+                        <PointerLockControls />
+                        <ambientLight color='white' intensity={0.3} />
+                        <Player />
+                        <Floor />
+                    </Physics>
+                </Suspense>
             </Canvas>
         </div>
     )
