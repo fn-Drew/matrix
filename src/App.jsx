deac63e3dd2bc04aa8ac4d9e440ad961e3dd714e
fn-Drew <alutter03@gmail.com>
2023-02-24 21:38:05 -0500

    attatched camera to physics object

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import { Canvas, } from '@react-three/fiber'
-import { PointerLockControls } from '@react-three/drei'
-import { Suspense } from 'react'
-import { Physics, RigidBody } from '@react-three/rapier'
+import { Canvas, useFrame, useThree, } from '@react-three/fiber'
+import { PointerLockControls, useKeyboardControls } from '@react-three/drei'
+import React, { Suspense, useRef, useEffect } from 'react'
+import { Physics, RigidBody, RapierRigidBody, CapsuleCollider, Debug } from '@react-three/rapier'
+import { PerspectiveCamera } from 'three'
 
 
 function Floor() {
-
     return (
         <RigidBody>
             <mesh position={[0, -2, 0]} rotation={[-Math.PI / 2, 0, 0]}>
@@ -14,37 +14,42 @@ function Floor() {
             </mesh >
         </RigidBody >
     )
-
 }
 
 function Player() {
+    const rigidBody = useRef()
+
+    useFrame((state) => {
+
+        if (rigidBody.current) {
+            let playerPosition = rigidBody.current.translation()
+            state.camera.position.set(playerPosition.x, playerPosition.y, playerPosition.z)
+        }
+
+    })
 
     return (
-        <RigidBody>
-            <mesh >
-                <sphereGeometry />
-                <meshStandardMaterial color='orange' />
-            </mesh>
+        <RigidBody ref={rigidBody} >
+            <CapsuleCollider args={[.75, .5]} />
         </RigidBody>
     )
-
 }
 
 function App() {
-
     return (
         <div id="canvas-container">
             <Canvas >
+                <PointerLockControls />
                 <Suspense>
                     <Physics gravity={[0, -9.8, 0]}>
-                        <PointerLockControls />
+                        <Debug />
                         <ambientLight color='white' intensity={0.3} />
                         <Player />
                         <Floor />
                     </Physics>
                 </Suspense>
             </Canvas>
-        </div>
+        </div >
     )
 }
 
